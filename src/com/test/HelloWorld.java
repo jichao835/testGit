21504acb769e3ca6f2e73f1ef86ba140a7043ca2
jichao <jichao835@sina.com>
2017-12-21 19:52:17 +0800

    no message

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -6,7 +6,7 @@ package com.test;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");//aaabbbcccddddddeee
+        System.out.println("hello world!");//aaabbbcccddddddfff
     }
 
 }
