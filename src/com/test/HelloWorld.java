6ba01b4d4240380015bd29ba8d48e37ede7acca0
jichao <jichao835@sina.com>
2017-12-21 17:48:53 +0800

    Initial commit

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by jichao on 2017/12/21.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
